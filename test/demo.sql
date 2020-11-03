88a6e9a4d7874f069d7ea0a85ed96e0859a5fc89
jun.chen <jun.chen@baisonmail.com>
2020-11-03 16:20:15 +0800

    测试提交

---
+++ test/demo.sql
@@ -0,0 +1,99 @@
+{
+    "name": "BUSINESS_BRAND",
+    "comment": "品牌档案",
+    "primaryKey": "ID",
+    "primaryKeyName": "SYS_C0012612",
+    "columns": [
+        {
+            "name": "ID",
+            "comment": "品牌ID",
+            "column_type": "NUMBER",
+            "null_able": false
+        },
+        {
+            "name": "DISABLEBY",
+            "comment": "停用人",
+            "column_type": "NVARCHAR2",
+            "null_able": true
+        },
+        {
+            "name": "MODIFYBY",
+            "comment": "修改人",
+            "column_type": "NVARCHAR2",
+            "null_able": true
+        },
+        {
+            "name": "MODIFYTIME",
+            "comment": "修改时间",
+            "column_type": "DATE",
+            "null_able": true
+        },
+        {
+            "name": "NAME",
+            "comment": "品牌名称",
+            "column_type": "NVARCHAR2",
+            "null_able": true
+        },
+        {
+            "name": "DISABLEDATE",
+            "comment": "停用时间",
+            "column_type": "DATE",
+            "null_able": true
+        },
+        {
+            "name": "STATUS",
+            "comment": "品牌状态0-初始，1-启用，2-停用",
+            "column_type": "NVARCHAR2",
+            "null_able": true
+        },
+        {
+            "name": "CODE",
+            "comment": "品牌代码",
+            "column_type": "NVARCHAR2",
+            "null_able": true
+        },
+        {
+            "name": "ISDEFAULT",
+            "comment": null,
+            "column_type": "NVARCHAR2",
+            "null_able": true
+        },
+        {
+            "name": "CREATECHANNELID",
+            "comment": "创建渠道ID",
+            "column_type": "NUMBER",
+            "null_able": true
+        },
+        {
+            "name": "CONTROLLER",
+            "comment": null,
+            "column_type": "NVARCHAR2",
+            "null_able": true
+        },
+        {
+            "name": "CREATEBY",
+            "comment": "创建人",
+            "column_type": "NVARCHAR2",
+            "null_able": true
+        },
+        {
+            "name": "ENABLEDATE",
+            "comment": "启用时间",
+            "column_type": "DATE",
+            "null_able": true
+        },
+        {
+            "name": "CREATETIME",
+            "comment": "创建渠道",
+            "column_type": "DATE",
+            "null_able": true
+        },
+        {
+            "name": "ENABLEBY",
+            "comment": "启用人",
+            "column_type": "NVARCHAR2",
+            "null_able": true
+        }
+    ],
+    "index": []
+}
